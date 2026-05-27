5960b5ff435e8440e8e9e2c48b2d93dd63fb07cb
2025130006 <kih050615@naver.com>
2026-05-27 16:50:10 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -4,16 +4,17 @@ int main(void)
 {
     char arr[10] = {};
     char *p = nullptr;
+    char *q = nullptr;
     
-    for (p = arr ; p < arr + 10; *p++) scanf ("%c", p);
+    for (p = arr ; p < arr + 10; p++) scanf ("%c", p);
     
     char max_char = '\0';
     int max_count = 0;
     
-    for (p = arr; p < arr + 10 ; *p++) {
+    for (p = arr; p < arr + 10 ; p++) {
         int current_count = 0;
         
-        for (char *q = arr ; q < arr + 10 ; *q++) {
+        for (q = arr ; q < arr + 10 ; q++) {
             if (*p == *q) current_count++;
         }
         if (current_count > max_count) {
