7fcbc8ad1a5b7e298b9cb1020c3788e0849c7489
2025130006 <kih050615@naver.com>
2026-05-27 16:49:19 +0900

    Update main.c

--- p2/main.c
+++ p2/main.c
@@ -2,28 +2,24 @@
 
 int main(void)
 {
-    char arr[10] = {};
-    char *p = nullptr;
-    char *q = nullptr;
-    
-    for (p = arr ; p < arr + 10; p++) scanf ("%c", p);
-    
-    char max_char = '\0';
-    int max_count = 0;
-    
-    for (p = arr; p < arr + 10 ; p++) {
-        int current_count = 0;
-        
-        for (q = arr ; q < arr + 10 ; q++) {
-            if (*p == *q) current_count++;
-        }
-        if (current_count > max_count) {
-        max_count = current_count;
-        max_char = *p;
-        }
-    }
-        
-    printf("%c %d", max_char , max_count);
-        
-    return 0;
+    int N;
+    scanf("%d", &N);
+    
+    int arr1[20] = {};
+    int arr2[20] = {};
+    
+    int *p = nullptr;
+    for (p = arr1 ; p < arr1 + N; p++) scanf("%d", p);
+    
+    int *q = nullptr;
+    for (q = arr2 ; q < arr2 + N; q++) scanf("%d", q);
+
+    int sum = 0;
+    
+    for (p = arr1, q = arr2 + N - 1 ; p < arr1 + N ; p++, q--) {
+        sum = *p + *q;
+        printf(" %d", sum);
+    }
+    
+    return 0;
 }
