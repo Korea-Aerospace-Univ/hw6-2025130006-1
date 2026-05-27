f79af5d3008fea0fb5dbf78804efe711e627d581
2025130006 <kih050615@naver.com>
2026-05-27 16:36:27 +0900

    Update main.c

--- p2/main.c
+++ p2/main.c
@@ -9,14 +9,14 @@ int main(void)
     int arr2[20] = {};
     
     int *p = nullptr;
-    for (p = arr1 ; p < arr1 + N; *p++) scanf("%d", p);
+    for (p = arr1 ; p < arr1 + N; p++) scanf("%d", p);
     
     int *q = nullptr;
-    for (q = arr2 ; q < arr2 + N; *q++) scanf("%d", q);
+    for (q = arr2 ; q < arr2 + N; q++) scanf("%d", q);
 
     int sum = 0;
     
-    for (p = arr1, q = arr2 + N - 1 ; p < arr1 + N ; *p++, *q--) {
+    for (p = arr1, q = arr2 + N - 1 ; p < arr1 + N ; p++, q--) {
         sum = *p + *q;
         printf(" %d", sum);
     }
