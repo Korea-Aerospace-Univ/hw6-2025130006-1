e45d245416da066910173a90c2b36be78ec9eeea
2025130006 <kih050615@naver.com>
2026-05-27 15:24:02 +0900

    Update main.c

---
+++ p1/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char arr[10] = {};
+    char *p = arr;
+    
+    for (int i = 0 ; i < 10; i++) scanf ("%c", p + i);
+    
+    char max_char = *arr;
+    int max_count = 0;
+    
+    for (char *p = arr ; p < arr + 10 ; p++) {
+        int current_count = 0;
+        
+        for (char *q = arr ; q < arr + 10 ; q++) {
+            if (*p == *q) current_count++;
+        }
+        if (current_count > max_count) {
+        max_count = current_count;
+        max_char = *p;
+        }
+    }
+        
+    printf("%c %d", max_char , max_count);
+        
+    return 0;
+}
